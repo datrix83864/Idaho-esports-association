ca857091a21f9b64e596ce0e88d17f98f8aff8ea
Dalton Hawkins <73982294+datrix83864@users.noreply.github.com>
2023-03-06 22:38:06 -0800

    update to publish

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'datrix83864', // Usually your GitHub org/user name.
-  projectName: 'esports-helpdesk', // Usually your repo name.
+  projectName: 'esports-idaho', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/datrix83864/Esports-HelpDesk',
+            'https://github.com/datrix83864/Esports-Idaho/blob/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/datrix83864/Esports-HelpDesk',
+            'https://github.com/datrix83864/Esports-Idaho/blob/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
