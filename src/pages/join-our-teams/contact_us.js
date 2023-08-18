b973d550af10d14e22c5333024f7fc8489153a26
datrix83864 <73982294+datrix83864@users.noreply.github.com>
2023-08-17 22:51:07 -0700

    fix: updated org name, dates, and some games

---
+++ src/pages/join-our-teams/contact_us.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+
+export default function Home() {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+      <Layout
+        title={`Contact Us`}
+        description="Contact the Idaho Esports Association">
+        <main>
+        <h1>Contact Us</h1>
+        <p>For general inquiries, please email <a href="mailto:info@idahoesports.gg">info@idahoesports.gg</a></p>
+        </main>
+      </Layout>
+    );
+  }
