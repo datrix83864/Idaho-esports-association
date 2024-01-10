24d8658b19d7084c123afbde705093ad3e5d46c8
datrix83864 <73982294+datrix83864@users.noreply.github.com>
2024-01-09 21:26:10 -0800

    feat: coaching, conflict, gamestop statements
    Signed-off-by: datrix83864 <73982294+datrix83864@users.noreply.github.com>

---
+++ src/components/ConflictStatement/ConflictStatement.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ConflictStatement = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          IEA encourages you to attempt to resolve all conflicts with the
+          opposing team
+        </li>
+        <li>
+        If your attempts at resolving the conflict have been unsuccessful, please reach out to IEA via the <a href="https://idahoesports.leagueos.gg">LeagueOS Site</a> to explain your situation and we will settle the matter for you.
+        </li>
+        <li> Have screen shots or replays when possible.</li>
+        <li> All IEA rulings on disputes are final</li>
+
+      </ul>
+    </div>
+  );
+};
+
+export default ConflictStatement;
