6c47c96223f54cf7a0dd4a8202a1b073a726d877
datrix83864 <73982294+datrix83864@users.noreply.github.com>
2024-01-15 22:16:15 -0800

    fix: repairs after
    Signed-off-by: datrix83864 <73982294+datrix83864@users.noreply.github.com>

--- src/api/GitHubPages/CommitDate.tsx
+++ src/api/GitHubPages/CommitDate.tsx
@@ -7,7 +7,7 @@ const CommitDate = ({ game }) => {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        `https://api.github.com/repos/datrix83864/Esports-HelpDesk/commits?path=docs%2FRules%2F${encodeURIComponent(game)}.mdx&page=1&per_page=1`
+        `https://api.github.com/repos/datrix83864/Idaho-esports-association/commits?path=docs%2FRules%2F${encodeURIComponent(game)}.mdx&page=1&per_page=1`
       );
       const dateObj = new Date(result.data[0].commit.committer.date);
       const formattedDate = `${dateObj.toLocaleDateString()} ${dateObj.toLocaleTimeString()}`;
