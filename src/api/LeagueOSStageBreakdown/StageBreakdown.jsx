2e94e46633228f85f0a6eb171c88d8bc0f0de9ad
Dalton Hawkins <daltonhwes@gmail.com>
2024-05-06 16:45:54 -0700

    fix: adding DocusaurusContext back
    Signed-off-by: Dalton Hawkins <daltonhwes@gmail.com>

--- src/api/LeagueOSStageBreakdown/StageBreakdown.jsx
+++ src/api/LeagueOSStageBreakdown/StageBreakdown.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const StageBreakdown = (props) => {
   const { siteConfig: { customFields } } = useDocusaurusContext();
