60e6ae52c715ab3d227d6ccfc3ca82292971c4c2
Dalton Hawkins <daltonhwes@gmail.com>
2024-05-06 17:08:50 -0700

    fix: Make it display team names
    Signed-off-by: Dalton Hawkins <daltonhwes@gmail.com>

--- src/api/LeagueOSStageBreakdown/StageBreakdown.jsx
+++ src/api/LeagueOSStageBreakdown/StageBreakdown.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import TeamName from '../LeagueOSTeamName/TeamName';
 
 const StageBreakdown = (props) => {
   const { siteConfig: { customFields } } = useDocusaurusContext();
@@ -8,7 +9,6 @@ const StageBreakdown = (props) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         const response = await axios.get(`https://api.leagueos.gg/seasons/${props.seasonId}/stages/${props.stageId}/matches`, {
@@ -16,9 +16,7 @@ const StageBreakdown = (props) => {
             'x-leagueos-api-key': customFields.apiKey,
           },
         });
-        console.log(response);
 
-        // Check if the response has a 'results' property and 'teams' array
         if (response.data) {
           setMatchData(response.data.data.results);
         } else {
@@ -27,7 +25,7 @@ const StageBreakdown = (props) => {
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
-        setLoading(false); // Set loading to false regardless of success or failure
+        setLoading(false);
       }
     };
 
@@ -35,30 +33,30 @@ const StageBreakdown = (props) => {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>; // Display loading indicator
+    return <div>Loading...</div>;
   }
 
-  // Sort the match data by match ID
   const sortedResults = matchData.sort((a, b) => a.matchId - b.matchId);
 
   return (
     <div>
-      <h2>Sorted Results</h2>
       <ul>
         {sortedResults.map((result) => {
-          // Converting UNIX timestamp to local timezone
           const date = new Date(result.date * 1000).toLocaleString();
 
           return (
             <li key={result.id}>
               <div>
-                <strong>Match ID:</strong> {result.matchId}
+                <strong>Match Name:</strong> {result.name}
               </div>
               <div>
                 <strong>Date:</strong> {date}
               </div>
               <div>
-                <strong>Name:</strong> {result.name}
+                <strong>Team 1:</strong> <TeamName teamId={result.teamIds[0]} />
+              </div>
+              <div>
+                <strong>Team 2:</strong> <TeamName teamId={result.teamIds[1]} />
               </div>
             </li>
           );
