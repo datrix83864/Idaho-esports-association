a32ef62e1676347cfe294d2ff92b11d4d83943e4
Dalton Hawkins <daltonhwes@gmail.com>
2025-01-07 12:34:07 -0800

    feat: season start kickoff display
    Signed-off-by: Dalton Hawkins <daltonhwes@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,15 @@ export default function Home() {
       description="The place for all things Idaho Esports!">
       <HomepageHeader />
       <main>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1545 2000">
+          <image width="1545" height="2000" xlink:href="/path/to/image"></image> <a xlink:href="https://youtube.com/live/7EmjJNinpG8?feature=share">
+            <rect x="373" y="1289" fill="#fff" opacity="0" width="315" height="385"></rect>
+          </a><a xlink:href="https://www.twitch.tv/idaho_esports">
+            <rect x="859" y="1290" fill="#fff" opacity="0" width="328" height="382"></rect>
+          </a><a xlink:href="https://www.idahoesports.gg">
+            <rect x="552" y="1786" fill="#fff" opacity="0" width="448" height="100"></rect>
+          </a>
+        </svg>
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 1080">
           <image width="1920" height="1080" xlinkHref={require('@site/static/img/HS_Game_Days.png').default}></image> <a xlinkHref="https://www.idahoesports.gg/schedule/smash_bros">
             <rect x="0" y="419" fill="#fff" opacity="0" width="402" height="307"></rect>
