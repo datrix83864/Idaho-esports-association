142cf3212ea0399766294c7b5e9ab232e6f7ddd6
Dalton Hawkins <daltonhwes@gmail.com>
2025-01-24 13:00:11 -0800

    feat: temp signup button fix
    Signed-off-by: Dalton Hawkins <daltonhwes@gmail.com>

---
+++ src/components/SignupForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignupForm() {
+  return (
+    <>
+      <button onClick={() => window.location.href="https://dashboard.mailerlite.com/forms/1282435/143994412526995129/share"} className="btn btn-primary btn-lg">Join our mailing list</button>
+    </>
+  );
+}
